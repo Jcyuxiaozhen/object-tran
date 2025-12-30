f7920d1b7a9992355d9065e4376e0edb7fdce8c4
JCyuxiaozhen <962730969@qq.com>
2025-12-30 10:54:20 +0800

    first commit

---
+++ tran/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+struct  book
+{
+    /* data */
+    unsigned int bookid;
+    char bookname[50];
+    char author[50];
+    unsigned int booknumber;
+};
+
+int main()
+{
+
+    
+    return 0;
+}
